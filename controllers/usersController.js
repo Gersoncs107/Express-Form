ee305cb263d1404b3597c04cc4652199ed2de5fd
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-25 22:46:10 -0300

    ExpressValidator

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,15 @@ const {body, validationResult} = require("express-validator")
 const alphaErr = "must only contain letters."
 const lengthErr = "must be between 1 and 10 characters."
 
+const validateUser = [
+    body("firstName").trim()
+      .isAlpha().withMessage(`First name ${alphaErr}`)
+      .isLength({ min: 1, max: 10 }).withMessage(`First name ${lengthErr}`),
+    body("lastName").trim()
+      .isAlpha().withMessage(`Last name ${alphaErr}`)
+      .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+  ]
+
 exports.usersListGet = (req, res) => {
     res.render("index",{
         title: "User List",
