fb4248e86bf620cb01c848f1990ed5170a9e3e93
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-25 22:32:59 -0300

    ExpressValidator

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,9 @@
 const usersStorage = require("../storages/usersStorage")
 const {body, validationResult} = require("express-validator")
 
+const alphaErr = "must only contain letters."
+const lengthErr = "must be between 1 and 10 characters."
+
 exports.usersListGet = (req, res) => {
     res.render("index",{
         title: "User List",
