9da4c7c9d4ac4345a2d832168e2c1b84deab7967
Gersoncs107 <gerson.silva107@gmail.com>
2025-03-28 22:40:09 -0300

    ExpressValidator

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,6 @@
 const { Router } = require("express")
-const usersController = require("../controllers/usersController")
+const usersController = require("../controllers/usersController");
+console.log(usersController)
 const usersRouter = Router()
 
 usersRouter.get("/", usersController.usersListGet)
